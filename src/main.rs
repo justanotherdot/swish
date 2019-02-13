ff75f84001c1382dd17f22286eb2801335e156c6
Ryan James Spencer <spencer.ryanjames@gmail.com>
2019-02-13 20:06:42 +1100

    Update TODO

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,8 @@ extern crate ring;
 use ring::digest;
 
 fn main() -> std::io::Result<()> {
-    // TODO Consider SHA256
+    // TODO Consider SHA256 instead
+    // This is just using the same as what git would do.
     let actual = digest::digest(&digest::SHA1, b"some line");
     let hex_digest = hex::encode(actual.as_ref());
 
