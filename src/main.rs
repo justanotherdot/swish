07074d6fe222786989fa83364895d89509e31055
Ryan James Spencer <justanotherdot@users.noreply.github.com>
2019-02-13 21:15:15 +1100

    Parse args (#7)
    This resolves #3.

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,36 @@
+extern crate clap;
 extern crate hex;
 extern crate ring;
 
+use clap::{App, Arg};
 use ring::digest;
 use std::io::{self, Read};
 
 fn main() -> std::io::Result<()> {
+    let matches = App::new("swish")
+        .version("0.1.0")
+        .author("Ryan James Spencer <spencer.ryanjames@gmail.com>")
+        .about("swish deterministically compares sets of files")
+        .arg(
+            Arg::with_name("verbose")
+                .short("v")
+                .long("verbose")
+                .help("Turns on verbose output"),
+        )
+        // TODO This will parse a single arg.
+        // But it's probably better if we require it's greater than two.
+        .arg(
+            Arg::with_name("INPUT")
+                .help("files to compare")
+                .multiple(true)
+                .required(true),
+        )
+        .get_matches();
+    println!("{:?}", matches);
+
+    // Read from stdin.
+    // Eventually this will be file based
+    // but supporting stdin appropriately e.g. `-f -`
     let mut buffer = String::new();
     io::stdin().read_to_string(&mut buffer)?;
 
