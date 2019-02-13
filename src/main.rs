14ebc9efff6421dfba3a06f21ee44cdde06812a3
Ryan James Spencer <spencer.ryanjames@gmail.com>
2019-02-13 20:40:06 +1100

    Read from stdin

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,15 @@ extern crate hex;
 extern crate ring;
 
 use ring::digest;
+use std::io::{self, Read};
 
 fn main() -> std::io::Result<()> {
+    let mut buffer = String::new();
+    io::stdin().read_to_string(&mut buffer)?;
+
     // TODO Consider SHA256 instead
     // This is just using the same as what git would do.
-    let actual = digest::digest(&digest::SHA1, b"some line");
+    let actual = digest::digest(&digest::SHA1, buffer.as_ref());
     let hex_digest = hex::encode(actual.as_ref());
 
     println!("{:x?}", hex_digest);
