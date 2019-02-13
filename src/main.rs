a40c46e534bf39f8a17c030cacb840794ed631df
Ryan James Spencer <spencer.ryanjames@gmail.com>
2019-02-13 19:52:18 +1100

    Reduce example

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,14 @@
 extern crate hex;
 extern crate ring;
 
-use ring::{digest, test};
+use ring::digest;
 
 fn main() -> std::io::Result<()> {
-    let expected_hex = "09ca7e4eaa6e8ae9c7d261167129184883644d07dfba7cbfbc4c8a2e08360d5b";
-    let expected: Vec<u8> = test::from_hex(expected_hex).unwrap();
-    // TODO swap SHA256 out with SHA1 possibly for speed
-    // although SHA2 is probably fine (100ms difference in some benchmarks)
-    let actual = digest::digest(&digest::SHA256, b"hello, world");
-    let actual_hex = hex::encode(actual.as_ref());
-    assert_eq!(&expected, &actual.as_ref());
+    // TODO Consider SHA256
+    let actual = digest::digest(&digest::SHA1, b"some line");
+    let hex_digest = hex::encode(actual.as_ref());
 
-    println!("{:x?}", actual_hex);
+    println!("{:x?}", hex_digest);
 
     Ok(())
 }
