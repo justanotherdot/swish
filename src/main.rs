bcf148ed443212a03a35dad91bb3e5b537b6bad3
Ryan James Spencer <spencer.ryanjames@gmail.com>
2019-02-20 20:48:51 +1100

    Refactor main module
    This closes #10 as I've realised the delimiter being passed as an arg
    isn't really needed.

--- src/main.rs
+++ src/main.rs
@@ -3,33 +3,38 @@ extern crate hex;
 extern crate rayon;
 extern crate ring;
 
-use clap::{crate_name, App, Arg};
+use clap::{crate_name, App, Arg, ArgMatches};
 use rayon::prelude::*;
 use ring::digest;
 use std::fs;
 
-fn hash_file(buffer: String) -> String {
-    let lines = buffer.split("\n").collect::<Vec<_>>();
-    let mut shas = lines
-        .par_iter()
-        .map(|s| hex::encode(digest::digest(&digest::SHA256, s.as_ref()).as_ref()))
-        .collect::<Vec<_>>();
-    shas.sort();
-    for sha in shas.iter() {
-        println!("hash: {}", sha);
-    }
+fn par_hash_lines(lines: Vec<&str>) -> Vec<String> {
+    lines.par_iter().map(|s| to_hex_digest(s)).collect()
+}
 
-    let actual = digest::digest(&digest::SHA256, shas.join("").as_ref());
-    let hex_digest = hex::encode(actual.as_ref());
+fn to_hex_digest(s: &str) -> String {
+    let digest = digest::digest(&digest::SHA256, s.as_ref());
+    hex::encode(digest.as_ref())
+}
 
-    println!("");
-    println!("final hash: {}", hex_digest);
-
+fn hash_file(buffer: &str) -> String {
+    let lines = buffer.lines().collect();
+    let mut shas = par_hash_lines(lines);
+    shas.par_sort();
+    let hex_digest = to_hex_digest(&shas.join(""));
     return hex_digest;
 }
 
-fn main() -> std::io::Result<()> {
-    let matches = App::new(crate_name!())
+fn process_files(files: Vec<&str>) -> Vec<String> {
+    files
+        .par_iter()
+        .map(|file| fs::read_to_string(file))
+        .map(|buffer| hash_file(&buffer.unwrap()))
+        .collect()
+}
+
+fn parse_opts<'a>() -> ArgMatches<'a> {
+    App::new(crate_name!())
         .version("0.1.0")
         .author("Ryan James Spencer <spencer.ryanjames@gmail.com>")
         .about("deterministically compares sets of files")
@@ -45,14 +50,16 @@ fn main() -> std::io::Result<()> {
                 .required(true)
                 .empty_values(false),
         )
-        .get_matches();
+        .get_matches()
+}
 
+fn main() -> std::io::Result<()> {
+    let matches = parse_opts();
+    // TODO This should probably be our own Config struct, or similar.
     let files = matches.values_of("FILE").unwrap().collect::<Vec<_>>();
-    for file in files.iter() {
-        let buffer = fs::read_to_string(file)?;
-        hash_file(buffer);
-        println!("");
-    }
+
+    let shas = process_files(files);
+    println!("{:?}", shas);
 
     // TODO This work can also be short-circuited by checking file lengths.
     //assert_eq!(hex_digest1, hex_digest2);
